9da14e89b4268f4197dde4dd62a9d1e5e799d9ef
BadBoy-Github <elayabarathiedison@gmail.com>
2026-01-16 18:29:34 +0530

    theme toggle button finished

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let parent = document.getElementById("parent");
 let btnToggle = document.getElementById("btnToggle");
 let cardParent = document.getElementById("cardParent");
 let heading = document.getElementById("heading");
+let btnRound = document.getElementById("btnRound");
 
 
 let isLight = true;
@@ -15,13 +16,15 @@ const toggleTheme = () => {
         cardParent.style.backgroundColor = "#FFFFFF";
         btnToggle.style.backgroundColor = "#3B82F6";
         heading.style.color = "#0F172A";
+        btnRound.style.transform = "translateX(0px)";
         // text #E5E7EB
         // mtext #94A3B8
     } else {
         parent.style.backgroundColor = "#020617";
         cardParent.style.backgroundColor = "#0F172A";
         btnToggle.style.backgroundColor = "#2563EB";
-        heading.style.color = "#E5E7EB"
+        heading.style.color = "#E5E7EB";
+        btnRound.style.transform = "translateX(30px)";
         // text #0F172A
         // mtext #64748B
     }
